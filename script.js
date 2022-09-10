9199931a1dd0300bbca52ab56109f91dcad25a69
Faris <pchillheel@gmail.com>
2022-09-10 19:36:02 +0700

    loop is working, Win Lose Draw working

--- script.js
+++ script.js
@@ -10,111 +10,62 @@ function getComputerChoice() {
     }
 }
 
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+let win = 0;
+let lose = 0;
+let draw = 0;
+
 function playRound(playerSelection,computerSelection) {
-
-    // const playerSelection = prompt("Rock,Paper or Scissors").toLowerCase();
-    // const computerSelection = getComputerChoice();   
-
-    let win = 0;
-    let lose = 0;
-
     if (playerSelection==="paper" && computerSelection === "Rock") {
-        console.log (`You Won! ${playerSelection} beats ${computerSelection}`)
         win++
-        console.log(`Score: Win = ${win} Lose = ${lose}`)
+        return (`You Won! ${playerSelection} beats ${computerSelection}`)        
     } else if (playerSelection==="rock" && computerSelection === "Paper") {
-        console.log (`You Lose! ${computerSelection} beats ${playerSelection}`)
         lose++
-        console.log(`Score: Win = ${win} Lose = ${lose}`)
+        return (`You Lose! ${computerSelection} beats ${playerSelection}`)        
     } else if (playerSelection==="paper" && computerSelection === "Scissors") {
-        console.log (`You Lose! ${computerSelection} beats ${playerSelection}`)
         lose++
-        console.log(`Score: Win = ${win} Lose = ${lose}`)
+        return (`You Lose! ${computerSelection} beats ${playerSelection}`)                
     } else if (playerSelection==="scissors" && computerSelection === "Paper") {
-        console.log (`You Won! ${playerSelection} beats ${computerSelection}`)
         win++
-        console.log(`Score: Win = ${win} Lose = ${lose}`)
+        return (`You Won! ${playerSelection} beats ${computerSelection}`)                
     } else if (playerSelection==="rock" && computerSelection === "Scissors") {
-        console.log (`You Won! ${playerSelection} beats ${computerSelection}`)
         win++
-        console.log(`Score: Win = ${win} Lose = ${lose}`)
+        return (`You Won! ${playerSelection} beats ${computerSelection}`)                
     } else if (playerSelection==="scissors" && computerSelection === "Rock") {
-        console.log (`You Lose! ${computerSelection} beats ${playerSelection}`)
         lose++
-        console.log(`Score: Win = ${win} Lose = ${lose}`)
+        return (`You Lose! ${computerSelection} beats ${playerSelection}`)            
     } else if (playerSelection==="rock" && computerSelection === "Rock") {
-        console.log ("DRAW")
+        draw++
+        return ("DRAW")                
     } else if (playerSelection==="scissors" && computerSelection === "Scissors") {
-        console.log ("DRAW")
+        draw++
+        return ("DRAW")                
     } else if (playerSelection==="paper" && computerSelection === "Paper") {
-        console.log ("DRAW")
+        draw++
+        return ("DRAW")        
     } else {
         console.log ("Please input Rock,Paper,Scissors")
     }
 }
 
-const playerSelection = prompt("Rock,Paper or Scissors").toLowerCase();
-const computerSelection = getComputerChoice();
-
-console.log(playRound(playerSelection,computerSelection));
-
-// function game() {
-//     // need to figuring out how to loop <Done>
-//     // already know how to store win and lose <Done>
-//     // need to figuring out how to put a new prompt after one loop
+function game() {
+    let roundCount = 0;
     
-//     let win = 0;
-//     let lose = 0;
-//     let roundCount = 0;
+    for(let roundCount=0; roundCount<5; roundCount++) {
+    playRound(prompt("Rock,Paper or Scissors").toLowerCase(),getComputerChoice())
+    }     
 
-//     // const playerSelection = prompt("Rock,Paper or Scissors").toLowerCase();
-//     // const computerSelection = getComputerChoice();
-//     for(let roundCount=0; roundCount<5; roundCount++) {
-        
-//         if (playerSelection==="paper" && computerSelection === "Rock") {
-//             console.log (`You Won! ${playerSelection} beats ${computerSelection}`)
-//             win++
-//             console.log(`Score: Win = ${win} Lose = ${lose}`)
-            
-//         } else if (playerSelection==="rock" && computerSelection === "Paper") {
-//             console.log (`You Lose! ${computerSelection} beats ${playerSelection}`)
-//             lose++            
-//             console.log(`Score: Win = ${win} Lose = ${lose}`)
-            
-//         } else if (playerSelection==="paper" && computerSelection === "Scissors") {
-//             console.log (`You Lose! ${computerSelection} beats ${playerSelection}`)
-//             lose++
-//             console.log(`Score: Win = ${win} Lose = ${lose}`)
-            
-//         } else if (playerSelection==="scissors" && computerSelection === "Paper") {
-//             console.log (`You Won! ${playerSelection} beats ${computerSelection}`)
-//             lose++            
-//             console.log(`Score: Win = ${win} Lose = ${lose}`)
-            
-//         } else if (playerSelection==="rock" && computerSelection === "Scissors") {
-//             console.log (`You Won! ${playerSelection} beats ${computerSelection}`)
-//             win++            
-//             console.log(`Score: Win = ${win} Lose = ${lose}`)
-            
-//         } else if (playerSelection==="scissors" && computerSelection === "Rock") {
-//             console.log (`You Lose! ${computerSelection} beats ${playerSelection}`)
-//             lose++            
-//             console.log(`Score: Win = ${win} Lose = ${lose}`)
-            
-//         } else if (playerSelection==="rock" && computerSelection === "Rock") {
-//             console.log ("DRAW")
-//         } else if (playerSelection==="scissors" && computerSelection === "Scissors") {
-//             console.log ("DRAW")
-//         } else if (playerSelection==="paper" && computerSelection === "Paper") {
-//             console.log ("DRAW")
-//         } else {
-//             console.log ("Please input Rock,Paper,Scissors")
-//         }
-//     } 
-    
-// }
+    if (win>lose && win>draw) {
+        console.log (`THE CHAMP IS HERE!! Score: Win = ${win} Lose = ${lose} Draw = ${draw}`)
+    } else if (lose>win && lose>draw) {
+        console.log (`I WANT REMATCH!! Score: Win = ${win} Lose = ${lose} Draw = ${draw}`)
+    } else {
+        console.log (`DRAW!! Score: Win = ${win} Lose = ${lose} Draw = ${draw}`)
+    }
+}
 
-// game();
+//still need to figure out if I can call playRound inside game() with playerSelection and computerSelection
 
-
-
+game();
